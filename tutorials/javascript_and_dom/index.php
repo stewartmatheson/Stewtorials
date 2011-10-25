b8c211a0cb7ea841c78bf7cbef5a48a8f4104efd
Stewart Matheson <stewart.matheson@gmail.com>
2011-10-25 15:28:00 +1100

    Spelling and Grammar fixes:

--- tutorials/javascript_and_dom/index.php
+++ tutorials/javascript_and_dom/index.php
@@ -3,12 +3,12 @@
 <h1>Javascript and the DOM</h1>
 
 <h3 class="add_to_toc">Document Object Model</h3>
-<p>Javascript's most common use is interacting with the document object model. The "DOM" is a tree structure that is built from HTML elements on a given web page. Each element is one branch on the tree. Branches on this tree can have other branches. Much is the same way files and folders work. These relationships are called child-parent relationships. A given element can have one parent and can also have many children. The HTML element is the only element in a given page that does not have a parent. It is the top most element and as such is referred to as the root of the tree.</p>
+<p>Javascript's most common use is interacting with the document object model. The "DOM" is a tree structure that is built from HTML elements on a given web page. Each element is one branch on the tree. Branches on this tree can have other branches. Much as the same way files and folders work. These relationships are called child-parent relationships. A given element can have one parent and can also have many children. The HTML element is the only element in a given page that does not have a parent. It is the top most element and as such is referred to as the root of the tree.</p>
 
-<p>Understanding the DOM is an important part of learning javascript and how to add interactivity to web pages. With out a firm understanding of the DOM you will be unable to write dynamic web pages.</p>
+<p>Understanding the DOM is an important part of learning JavaScript and how to add interactivity to web pages. Without a firm understanding of the DOM you will be unable to write dynamic web pages.</p>
 
 <h3 class="add_to_toc">DOM: Elements</h3>
-<p>As discussed the DOM is made up of elements. Javascript performs actions on these DOM elements. This allows us to change the page in any way we need even after a page has been loaded. Couple with this with an events system and we can create fully functional interactive experiences.</p>
+<p>As discussed the DOM is made up of elements. Javascript performs actions on these DOM elements. This allows us to change the page in any way we need even after a page has been loaded. Coupled with this is an events system we can use to create fully functional and interactive experiences.</p>
 
 <h3 class="add_to_toc">Exercise: Explore the DOM</h3>
 <div class="exercise">
@@ -21,11 +21,11 @@
 </div>
 
 <h3 class="add_to_toc">jQuery</h3>
-<p>Javascript is can be used to control the DOM however the design is not great. Developers end up sending a lot of time rewriting the same code again and again. There is also cross browser issues to consider. What works in one browser might not work in another with HTML and this is also true of Javascript. There are many frameworks that are available out there that that help javascript developers. One such framework is <a href="http://jquery.com">jQuery</a>.</p>
+<p>Javascript can be used to control the DOM. However the design of JavaScript around interacting with the DOM is poor. Developers end up spending a lot of time rewriting the same code. There are also cross browser issues to consider. What works in one browser might not work in another. There are many frameworks that are available out there that that help JavaScript developers. One such framework is <a href="http://jquery.com">jQuery</a>.</p>
 
-<p>jQuery makes writing javascript a much more enjoyable experience.  jQuery aims to simplify some of the more common tasks in javascript development while unifying  the experience over the different available browsers. This is very handy as it allows developers to be more productive. jQuery is used widely in industry and is open and free. Some of jQuery's features include</p>
+<p>jQuery makes writing JavaScript a much more enjoyable experience.  jQuery aims to simplify some of the more common tasks in JavaScript development. jQuery also unifies the experience over different browsers. This is handy as it allows developers to be more productive. jQuery is used widely in industry and is open and free.</p>
 
-<p>To use jQuery we just have to include the jQuery library file in our source code of our page. Google host jQuery and a number of other handy javascript library for us. This is handy as we don't have to download the library and set it up each time. We can just include a link to google's hosted version. The following example is how to do this.</p>
+<p>To use jQuery we must include the jQuery library file in our source code of our page. Google host jQuery and a number of other handy JavaScript libraries for us. This is handy as we don't have to download the library and set it up each time. We can just include a link to Google's hosted version. The following example is how to do this.</p>
 
 <?php render_code_block('html', "include_jquery.html", "Including jQuery in a page."); ?>
 
@@ -56,7 +56,7 @@
 </div>
 
 <h3 class="add_to_toc">Document Ready</h3>
-<p>One of the most common tasks performed in jQuery is initialisation. This is an event that happens when the document is loaded in the browser. Any code that sets up other events such as mouse clicks and keyboard events should by run when the document is ready. Fortunately jQuery provides us with a way of binding events to document ready. Through out these tutorials you will need to always set up your jQuery code to run in on document ready otherwise it will not work</p>
+<p>One of the most common tasks performed in jQuery is initialisation. This is an event that fires when the document is loaded in the browser. Any code that sets up other events such as mouse clicks and keyboard events should be run when the document is ready. Fortunately jQuery provides us with a way of binding events to document ready. Through out these tutorials you will need to always set up your jQuery code to run on document ready otherwise it will not work</p>
 
 <?php render_code_block('js', "document_ready.js", "Binding events with document ready."); ?>
 
@@ -74,9 +74,10 @@
 
 
 <h3 class="add_to_toc">The jQuery Object</h3>
-<p>Performing actions on the DOM using jQuery is done via the jQuery object. The jQuery object is a javascript object with all of the functions contained with in jQuery. This global can be referenced in two different ways. Either by the work "jQuery" or by the "$" sign. The "$" sign is more common and makes for less code however its up to the writer as to what they want to use as both work.</p>
+<p>Performing actions on the DOM using jQuery is done via the jQuery object. The jQuery object is a JavaScript object with all of the functions contained within jQuery. This global can be referenced in two ways. Either by the word "jQuery" or by the "$" sign. The "$" sign is more common and makes for less code however its up to the writer as to what they want to use as both work.</p>
 
-<?php render_code_block('js', "jquery_object.js", "Using the javascript object"); ?>
+
+<?php render_code_block('js', "jquery_object.js", "Using the JavaScript object"); ?>
 
 <h3 class="add_to_toc">Exercise: jQuery Object</h3>
 <div class="exercise">
@@ -91,7 +92,7 @@
 	<ol>
 </div>
 
-<p>When using jQuery there are two main steps. First is working out the correct selector for your needs and the second is performing operations on that selector. Operations in jQuery are all functions that are callable form the jQuery object. When we use the jQuery object and type a selector then any more operations we perform on that object will be applied to all of the elements selected. As we are dealing with a single jQuery object we can also use method chaining to apply multiple operations</p>
+<p>When using jQuery there are two main steps. First is working out the correct selector for your needs and the second is performing operations on that selector. Operations in jQuery are functions that are callable from the jQuery object. When we use the jQuery object and type a selector then any more operations we perform on that object will be applied to all of the elements selected. As we are dealing with a single jQuery object we can also use method chaining to apply multiple operations.</p>
 
 <?php render_code_block('js', "jquery_operations.js", "jQuery operations and method chaining"); ?>
 
@@ -109,15 +110,15 @@
 	<ol>
 </div>
 
-<p>A final note on manipulation: jQuery is a big framework. Its work taking time out to read though the <a href="http://api.jquery.com">API</a>. Even if you don't read every part of it its still great to have an overview of how the framework functions and what it can offer.</p>
+<p>A final note on manipulation: jQuery is a big framework. Its worth taking time out to read through the <a href="http://api.jquery.com">API</a>. Even if you don't read every part of it its still great to have an overview of how the framework functions and what it can offer.</p>
 
 <h3 class="add_to_toc">Events</h3>
-<p>Unlike PHP and HTML javascript programming can be state based. The DOM represents a single state and can be changed at any given time. We can also save data in different parts of the DOM. This turns the tables and allows us to build web applications that are more like desktop applications. Events in jQuery normally accept a function as part of their parameters. This function is a closure and will be executed when jQuery is ready to call the event. This provides us with a way of hooking in to events and running code we want. It's a great way to program as we can bind as many functions to an event as we need. Thus allowing us to create a set of functions that all perform actions for a feature of our program. This practice is known as <a href="http://en.wikipedia.org/wiki/Aspect-oriented_programming">aspect orientated programming</a>.</p>
+<p>Unlike PHP and HTML JavaScript programming can be state based. The DOM represents a single state and can be changed at any given time. We can also save data in different parts of the DOM. This turns the tables and allows us to build web applications that are more like desktop applications. Events in jQuery normally accept a function as part of their parameters. This function is a closure(first level function) and will be executed when jQuery is ready to call the event. This provides us with a way of hooking in to events and running code. It's a great way to program as we can bind as many functions to an event as we need. Thus allowing us to create a set of functions that all perform actions for a feature of our program. This practice is known as <a href="http://en.wikipedia.org/wiki/Aspect-oriented_programming">aspect orientated programming</a>.</p>
 
 <?php render_code_block('js', "jquery_events.js", "Some basic events in jQuery."); ?>
 
 <h3 class="add_to_toc">Effects</h3>
-<p>The jQuery framework can be used to add effects to a page. This allows to to perform complex operations on elements like animation. As javascript in web browsers speeds up with google chrome the possibilities of interactivity with in web pages increases.</p>
+<p>The jQuery framework can be used to add effects to a page. This allows to to perform complex operations on elements like animation. As JavaScript in web browsers speeds up with Google Chrome the possibilities of interactivity with in web pages increases.</p>
 
 <p>jQuery makes these effects methods available via functions that can be applied to selectors. We have already used slideIn and slideOut so lets have a look at some other effects.</p>
 
@@ -138,7 +139,7 @@
 	<ol>
 </div>
 
-<p>There is much more that can be done with javascript and jQuery. Make sure to keep reading W3Schools and the jQuery documentation to stay on top of changes. The jQuery library in particular moves very quickly. If you have any suggestions please add them to the comments below.</p>
+<p>There is much more that can be done with JavaScript and jQuery. Make sure to keep reading W3Schools and the jQuery documentation to stay on top of changes. The jQuery library in particular moves very quickly. If you have any suggestions please add them to the comments below.</p>
 
 <?php include "../../disqus.php"; ?>
 <?php include "../../footer.php"; ?>
