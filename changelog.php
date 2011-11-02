24e234d36919c6647b5b09781815408e2b3129c2
Stewart Matheson <stewart.matheson@gmail.com>
2011-11-02 11:06:06 +1100

    added change log fixed mistake

---
+++ changelog.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+<h1>Changes</h1>
+
+<h3>2/11/2011</h3>
+
+<ul>
+    <li>Changed the last exercise in the first tutorial on IEEE floating point</li>
+    <li>Change log added</li>
+    <li>Fixed up some spelling in Exercise 1</li>
+</ul>
+
+
+<?php include 'footer.php'; ?>
